3d84c1eccea9064bfc3a3c1fdea7ac450963a061
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-11-19 16:34:00 -0600

    added in thye comments the proper way to reconfigure my app.js using react HOOKS

--- src/App.js
+++ src/App.js
@@ -47,3 +47,97 @@ class App extends Component {
 }
 
 export default App;
+
+
+/*
+///////////////
+// HOOKS EXAMPLE
+///
+
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app = props => {
+  //useState always returns an array with 2 elements
+  // first element is the current state passed in
+  // second is a function that will re render the component
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+  ]
+});
+
+//you are allowed to have AS MANY useState() calls as you want/need
+
+const [otherState, setOtherState] = useState('some other value');
+
+console.log(personsState, otherState);
+
+const switchNameHandler = () => {
+  //console.log("Was clicked!");
+  //DO NOT DO THIS ... this.state.persons[0].name = "EDWARDS!!";
+  setPersonsState({
+    //THIS FUNCTION REPLACES THE OLD STATE
+    //this react component method compares the 2 states and mutates accordingly
+    // merges the old with the new
+    // react recognizes that the state changes - so it re renders the DOM
+    persons: [
+      {name: 'Edward', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 21}
+    ],
+    //ensures we keep all data since our state is changed, and not merged with the old state
+    otherState: 'some other value'
+  }) 
+}
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app</h1>
+        <p>this works and stuff</p>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      </div>
+    ); 
+   // return React.createElement('div', null, 'h1', 'Hi, I\'m a react app!')
+
+}
+
+export default app;
+
+/*
+//managing compents internal data
+state = {
+  //WHEN STATE CHANGES IT RE RENDERS THE DOM
+  //state allows you to manage anything about the object you desire
+  persons: [
+    {name: 'Max', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Stephanie', age: 26}
+  ],
+  otherState: 'some other value'
+}
+
+//"Handler" naming convntion denotes that it is a function that is not called, but handles an event
+switchNameHandler = () => {
+  //console.log("Was clicked!");
+  //DO NOT DO THIS ... this.state.persons[0].name = "EDWARDS!!";
+  this.setState({
+    //this react component method compares the 2 states and mutates accordingly
+    // merges the old with the new
+    // react recognizes that the state changes - so it re renders the DOM
+    persons: [
+      {name: 'Edward', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 21}
+    ]
+  }) 
+}
+*/
+
+*/
