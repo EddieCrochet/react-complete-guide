f0f34bb6541520e16edab79f98f4699cbb6e0bae
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-11-18 19:11:53 -0600

    more playing with and setting up react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //managing compents internal data
   state = {
+    //WHEN STATE CHANGES IT RE RENDERS THE DOM
+    //state allows you to manage anything about the object you desire
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  //"Handler" naming convntion denotes that it is a function that is not called, but handles an event
+  switchNameHandler = () => {
+    //console.log("Was clicked!");
+    //DO NOT DO THIS ... this.state.persons[0].name = "EDWARDS!!";
+    this.setState({
+      //this react component method compares the 2 states and mutates accordingly
+      // merges the old with the new
+      // react recognizes that the state changes - so it re renders the DOM
+      persons: [
+        {name: 'Edward', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 21}
+      ]
+    })
   }
 
   render() {
@@ -16,7 +36,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p>this works and stuff</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
