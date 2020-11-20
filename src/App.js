75d3b9f7f7b7af5cfee93431fccd80264780a0ee
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-11-20 15:15:01 -0600

    more react learning, playing with css, now ont oa challenge

--- src/App.js
+++ src/App.js
@@ -42,11 +42,22 @@ class App extends Component {
   }
 
   render() {
+    //THE BELOW IS STILL JS
+    const style = {
+      backgroundColor: 'white',
+       font: 'inherit',
+       border: '1px solid blue',
+       padding: '8px',
+       cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p>this works and stuff</p>
-        <button onClick={() => this.switchNameHandler("Edwilliam!")}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler("Edwilliam!")}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
